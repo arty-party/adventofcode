32f5faa78a9dcc247aa82f67ca718d692b7c0c16
arty-party <mario.bairhuber@gmail.com>
2024-12-02 22:00:48 +0100

    typo

--- aoc_2024/day_01/solution_test.ts
+++ aoc_2024/day_01/solution_test.ts
@@ -4,24 +4,24 @@ import { caluclate_distance, find_similarity } from "./solution.ts";
 
 Deno.test("caluclate_distance", async (t) => {
   await t.step("Demo input", () => {
-    const input = readFromInput("demo_input", "1");
+    const input = readFromInput("demo_input", "01");
     assertEquals(caluclate_distance(input), 11);
   });
 
   await t.step("Solution input", () => {
-    const input = readFromInput("input", "1");
+    const input = readFromInput("input", "01");
     assertEquals(caluclate_distance(input), 2031679);
   });
 });
 
 Deno.test("find_similarity", async (t) => {
   await t.step("Demo input", () => {
-    const input = readFromInput("demo_input", "1");
+    const input = readFromInput("demo_input", "01");
     assertEquals(find_similarity(input), 31);
   });
 
   await t.step("Solution input", () => {
-    const input = readFromInput("input", "1");
+    const input = readFromInput("input", "01");
     assertEquals(find_similarity(input), 19678534);
   });
 });
--- aoc_2024/day_02/solution_test.ts
+++ aoc_2024/day_02/solution_test.ts
@@ -7,23 +7,23 @@ import {
 
 Deno.test("Check for safe reports", async (t) => {
   await t.step("Demo input", () => {
-    const input = readFromInput("demo_input", "2");
+    const input = readFromInput("demo_input", "02");
     assertEquals(checkForSafeReports(input), 2);
   });
 
   await t.step("Solution input", () => {
-    const input = readFromInput("input", "2");
+    const input = readFromInput("input", "02");
     assertEquals(checkForSafeReports(input), 230);
   });
 });
 
 Deno.test("Check for actually safe reports", async (t) => {
   await t.step("Demo input", () => {
-    const input = readFromInput("demo_input", "2");
+    const input = readFromInput("demo_input", "02");
     assertEquals(checkForActuallySafeReports(input), 4);
   });
   await t.step("Solution input", () => {
-    const input = readFromInput("input", "2");
+    const input = readFromInput("input", "02");
     assertEquals(checkForActuallySafeReports(input), 301);
   });
 });
